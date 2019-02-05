4a0b8d8ee10ba4155009a5cdca8bda18ce62c4cd
wrf <wfrancis@biology.sdu.dk>
2019-02-05 16:37:02 +0100

    erc data

---
+++ erc/erc_project_counts_plots.R
@@ -0,0 +1,177 @@
+
+
+library(stats)
+
+stg_eval = read.table("~/git/misc-analyses/erc/erc_2018_StG_evaluated_projects_all_panels.tab",header=TRUE,sep="\t",row.names=1)
+stg_grant = read.table("~/git/misc-analyses/erc/erc_2018_StG_granted_projects_all_panels.tab",header=TRUE,sep="\t",row.names=1)
+
+stg_eval
+stg_grant
+stg_N_countries = dim(stg_grant)[1]
+stg_N_yrs = dim(stg_eval)[2]
+stg_N_countries
+stg_N_yrs
+
+stg_granted_countries = match( row.names(stg_grant), row.names(stg_eval))
+stg_granted_countries
+
+stg_eval_sums = rev(colSums(stg_eval[,]))
+stg_grant_sums = rev(colSums(stg_grant[,]))
+stg_eval_sums
+
+country_cols = rainbow(stg_N_countries)
+
+plot(stg_grant_sums/stg_eval_sums, type='l', ylim=c(0,1), frame.plot=FALSE, axes=FALSE, ylab="Granted projects", xlab="Year", cex.lab=1.4, lwd=3)
+axis(2, cex.axis=1.3)
+axis(1,at=1:stg_N_yrs, label=rev(names(stg_grant)), cex.axis=1.3)
+for (i in 1:stg_N_countries) {
+	lines(1:stg_N_yrs, rev(stg_grant[i,])/rev(stg_eval[stg_granted_countries[i],]), col=country_cols[i])
+}
+
+###
+### BEGIN MAPS SECTION 
+###
+
+library(maps)
+
+stg_null_eval_sums = rep(0, dim(stg_eval)[1])
+stg_null_eval_sums[stg_granted_countries] = rowSums(stg_grant)
+stg_null_eval_sums
+rowSums(stg_grant)
+rowSums(stg_eval[stg_granted_countries,])
+rowSums(stg_eval)
+stg_countryaverage = stg_null_eval_sums/rowSums(stg_eval)
+stg_countryaverage
+range(stg_countryaverage)/min(stg_countryaverage)
+
+
+ga_colors = c( colorRampPalette(c("#8e0152","#ffffbf"))(12), colorRampPalette(c("#ffffbf","#006837"))(12) )
+plot(1:24,1:24,cex=5,pch=16,type="p",col=ga_colors)
+
+ga_colors
+
+
+floor(stg_countryaverage*100)
+colorby_ga = ga_colors[floor(stg_countryaverage*100)+1]
+colorby_ga
+
+latrange = c(30,62)
+lonrange = c(-10,39)
+
+euromap = map('world', ylim=latrange, xlim=lonrange)
+#attributes(euromap)
+
+stg_countries = row.names(stg_eval)
+stg_countries
+stg_countries[length(stg_countries)] = "UK:Great Britain"
+euronums = match(stg_countries, euromap$names)
+euronums
+#euronums[32] = 57
+euronumnoNA = euronums[!is.na(euronums)]
+countriesnoNA = stg_countries[!is.na(euronums)]
+
+!is.na(euronums)
+
+euromap$names[euronumnoNA]
+colorby_ga_noNA = colorby_ga[!is.na(euronums)]
+vecbycountry = rep("#ffffff", length(euromap$names) )
+vecbycountry[euronumnoNA] = colorby_ga_noNA
+vecbycountry[euronumnoNA]
+vecbycountry
+for (ctry in countriesnoNA){
+	vecbycountry[grep(ctry,euromap$names)] = colorby_ga_noNA[ grep(ctry,countriesnoNA)]
+} 
+
+grayscale = colorRampPalette(c("#999999","#eeeeee"))(4)
+
+pdf(file="~/git/misc-analyses/erc/erc_2018_StG_granted_projects_all_panels_ratio.pdf", width=8, height=8)
+# make map
+euromap = map('world', ylim=latrange, xlim=lonrange, fill=TRUE, col=vecbycountry, mar=c(1,1,1,1))
+# make legend in north africa
+rect(seq(-5, -5+length(ga_colors)-1 ,1),rep(32,length(ga_colors)), seq(-4,-4+length(ga_colors)-1,1) ,rep(33,length(ga_colors)),col=ga_colors)
+text(-5,34,"0", cex=2)
+text(-5+length(ga_colors)-1,34, length(ga_colors) , cex=2)
+rect(-9,30,20,32,border=FALSE,col="white")
+text(-5+(length(ga_colors)/2),31,"% ERC Starting Grants Awarded", cex=2)
+dev.off()
+
+
+
+cog_eval = read.table("~/git/misc-analyses/erc/erc_2018_CoG_evaluated_projects_all_panels.tab",header=TRUE,sep="\t",row.names=1)
+cog_grant = read.table("~/git/misc-analyses/erc/erc_2018_CoG_granted_projects_all_panels.tab",header=TRUE,sep="\t",row.names=1)
+
+
+cog_N_countries = dim(cog_grant)[1]
+cog_N_yrs = dim(cog_eval)[2]
+
+cog_granted_countries = match( row.names(cog_grant), row.names(cog_eval))
+cog_granted_countries
+cog_eval_sums = rev(colSums(cog_eval[,]))
+cog_grant_sums = rev(colSums(cog_grant[,]))
+
+cog_null_eval_sums = rep(0, dim(cog_eval)[1])
+cog_null_eval_sums[cog_granted_countries] = rowSums(cog_grant)
+cog_null_eval_sums
+rowSums(cog_eval)
+cog_countryaverage = cog_null_eval_sums/rowSums(cog_eval)
+cog_countryaverage
+max(cog_countryaverage)
+
+ga_colors = c( colorRampPalette(c("#8e0152","#ffffbf"))(16), colorRampPalette(c("#ffffbf","#313695"))(16) )
+plot(1:32,1:32,cex=5,pch=16,type="p",col=ga_colors)
+
+colorby_ga = ga_colors[floor(cog_countryaverage*100)+1]
+colorby_ga
+
+#attributes(euromap)
+
+cog_countries = row.names(cog_eval)
+cog_countries[length(cog_countries)] = "UK:Great Britain"
+euronums = match(cog_countries, euromap$names)
+euronums
+euronumnoNA = euronums[!is.na(euronums)]
+euronumnoNA
+countriesnoNA = cog_countries[!is.na(euronums)]
+countriesnoNA
+colorby_ga_noNA = colorby_ga[!is.na(euronums)]
+colorby_ga_noNA
+vecbycountry = rep("#ffffff", length(euromap$names) )
+vecbycountry[euronumnoNA] = colorby_ga_noNA
+for (ctry in countriesnoNA){
+	vecbycountry[grep(ctry,euromap$names)] = colorby_ga_noNA[ grep(ctry,countriesnoNA)]
+} 
+
+
+
+pdf(file="~/git/misc-analyses/erc/erc_2018_CoG_granted_projects_all_panels_ratio.pdf", width=8, height=8)
+# make map
+euromap = map('world', ylim=latrange, xlim=lonrange, fill=TRUE, col=vecbycountry, mar=c(1,1,1,1))
+# make legend in north africa
+rect(seq(-5, -5+length(ga_colors)/2-0.5 ,0.5),rep(32,length(ga_colors)), seq(-4,-4+length(ga_colors)/2-0.5,0.5) ,rep(33,length(ga_colors)),col=ga_colors)
+text(-5,34,"0", cex=2)
+text(-5+length(ga_colors)/2-1,34, length(ga_colors) , cex=2)
+rect(-8,30,24,32,border=FALSE,col="white")
+text(-8,31,"% Consolidator Grants Awarded", cex=2,pos=4)
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+adg_eval = read.table("~/git/misc-analyses/erc/erc_2018_AdG_evaluated_projects_all_panels.tab",header=TRUE,sep="\t",row.names=1)
+adg_grant = read.table("~/git/misc-analyses/erc/erc_2018_AdG_granted_projects_all_panels.tab",header=TRUE,sep="\t",row.names=1)
+
+#
