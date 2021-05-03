7fcc39f5681ba19e90ee84d1ae47d29ccea492c3
wrf <wfrancis@biology.sdu.dk>
2021-05-03 23:06:02 +0200

    fisheries shinyapp v1

---
+++ fisheries/shiny/app.R
@@ -0,0 +1,142 @@
+# fisheries/shiny/app.R
+# make interactive choropleth plot of fisheries data from the UN
+#
+# data at:
+# www.fao.org/fishery/statistics/global-consumption/en
+# http://www.fao.org/faostat/en/#data/FBS
+#
+# created by WRF 2021-05-03
+
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+
+# current host of this file at:
+fishdatafile = "~/git/misc-analyses/fisheries/data/FoodBalanceSheets_E_reformat_all_fish.tab"
+fishdata = read.table(fishdatafile, header=TRUE, sep="\t", stringsAsFactors = FALSE)
+#head(fishdata)
+
+worldpolygons = map_data("world")
+#head(worldpolygons)
+#unique(worldpolygons$region)
+
+# adjust names for certain remaining countries
+# most renaming is handled by the python script simplify_foodbalance_table.py
+fishdata$region = recode(fishdata$region,
+                         "Cote d'Ivoire" = "Ivory Coast",
+                         "Czechia" = "Czech Republic" )
+fishdata_recode = mutate(fishdata, net_human_consumed = production_2017_Gg + imports_2017_Gg - exports_2017_Gg - feed_2017_Gg,
+         import_v_prod = pmin(imports_2017_Gg / (production_2017_Gg+0.01),22.22) )
+#head(fishdata_recode)
+countries_w_data = sort(unique(fishdata_recode$region))
+
+ui <- fluidPage(
+  titlePanel("UN FAO Fisheries"),
+  verticalLayout(
+    fluidRow(
+      column(4,
+             sliderInput(inputId = "lat",
+                         label = "Latitude",
+                         min = -90,
+                         max = 90,
+                         value = c(-55,75)
+                        ),
+             sliderInput(inputId = "long",
+                         label = "Longitude",
+                         min = -180,
+                         max = 180,
+                         value = c(-180,180)
+                        )
+             ), # end column
+      column(4,
+             selectInput("fishtype", "Seafood type", 
+                         choices = list("All fish and seafood" = "Fish, Seafood", 
+                                        "Pelagic fish" = "Pelagic Fish", 
+                                        "Demersal fish" = "Demersal Fish",
+                                        "Other marine fish" = "Marine Fish, Other",
+                                        "Freshwater fish" = "Freshwater Fish", 
+                                        "Crustaceans" = "Crustaceans", 
+                                        "Cephalopods" = "Cephalopods",
+                                        "Molluscs (excl. cephalopods)" = "Molluscs, Other")
+                         ),
+             selectInput("displaytype", "Measurement", 
+                         choices = list("Imports" = "imports_2017_Gg", 
+                                        "Production" = "production_2017_Gg", 
+                                        "Exports" = "exports_2017_Gg",
+                                        "Use as animal feed" = "feed_2017_Gg", 
+                                        "Net food for humans" = "net_human_consumed", 
+                                        "Ratio of import/production" =  "import_v_prod"
+                                        )
+                        ),
+             helpText("Note: Net = imports + prod - exports - feed")
+             ), # end column
+      column(4,
+             selectInput("chosenCountry", "Country to display tabular data below", 
+                         choices = countries_w_data
+                         )
+             )
+    ), # end fluidRow
+
+    mainPanel(width="100%",
+              #verbatimTextOutput(outputId = "showingWhat"),
+              plotOutput(outputId = "worldMap",
+                 height="600px"
+                 ),
+              tableOutput("countryInfo")
+    ) # end mainPanel
+  ) # end verticalLayout
+)
+
+#
+server <- function(input, output) {
+  #output$showingWhat <- renderText({ print(input$fishtype) })
+  
+  output$worldMap <- renderPlot({
+    # subset fish data
+    fishdata_subset = left_join(worldpolygons, filter(fishdata_recode, item==input$fishtype), by="region" )
+    # choose the measurement to display
+    filldata = select(fishdata_subset, input$displaytype)[,1]
+    
+    # adjust parameters for the one measurement that is a ratio
+    if (input$displaytype=="import_v_prod") {
+      color_set = colorRampPalette(c("#4393c3", "#addd8e", "#d6604d"))(7)
+      color_bins = c(0.01, 0.05, 0.1, 0.5, 1, 5, 10 )
+    }
+    # otherwise just use light-green to dark-blue palette
+    else {
+      color_set = colorRampPalette(c("#b0e6cd","#063e6f"))(7)
+      color_bins = c(0, 1, 10, 100, 1000, 10000, 100000)
+    }
+    
+    # generate the map
+    fishgg = ggplot(fishdata_subset, aes(long,lat, group=group)) +
+      coord_cartesian(xlim=input$long, ylim=input$lat ) +
+      theme(plot.title = element_text(size=20),
+            axis.text = element_blank(),
+            axis.ticks = element_blank(),
+            legend.position=c(0,0.5),
+            legend.key.size = unit(1, 'cm'),
+            legend.justification = "left") +
+      scale_fill_gradientn(colours = color_set, 
+                           breaks = color_bins,
+                           na.value="gray70", trans = "log10") +
+      labs(x=NULL, y=NULL, fill="Quantity\n(M kg)",
+           subtitle= paste("showing:", input$fishtype ),
+           caption="Data from UN Food and Agriculture Organization (FAO) 2017\nwww.fao.org/fishery/statistics/global-consumption/en") +
+      geom_polygon( aes(x=long, y = lat, group = group, fill=filldata), colour="#ffffff")
+    fishgg
+
+  })
+  
+  output$countryInfo <- renderTable({
+    filter(fishdata_recode, region==input$chosenCountry)
+  },
+  hover = TRUE,
+  spacing = 'xs',
+  width = "100%"
+  )
+}
+
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
---
+++ fisheries/simplify_foodbalance_table.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+#
+
+'''simplify_foodbalance_table.py  last modified 2021-05-03
+
+    to take all seafood categories:
+./simplify_foodbalance_table.py -i FoodBalanceSheets_E_All_Data.csv > FoodBalanceSheets_E_reformat_all_fish.tab
+
+    to take only the totals, use -s :
+./simplify_foodbalance_table.py -i FoodBalanceSheets_E_All_Data.csv -s > FoodBalanceSheets_E_fish_summary_only.tab
+
+    data from UN FAO, for 2014 to 2017
+  www.fao.org/fishery/statistics/global-consumption/en
+  http://www.fao.org/faostat/en/#data/FBS
+    
+'''
+
+import sys
+import argparse
+import csv
+from collections import defaultdict
+
+def parse_balancetable_csv(csvfile, do_summary):
+	''''''
+	if do_summary:
+		fishery_allowed_items = ["Fish, Seafood"]
+	else:
+		fishery_allowed_items = ["Fish, Seafood", "Freshwater Fish", "Demersal Fish", "Pelagic Fish", "Marine Fish, Other", "Crustaceans", "Cephalopods", "Molluscs, Other"]
+	related_items = ["Aquatic Products, Other", "Aquatic Animals, Others", "Aquatic Plants"]
+
+	element_variation_terms = ["Production", "Import Quantity", "Stock Variation", "Export Quantity", "Domestic supply quantity", "Other uses (non-food)", "Food"]
+
+	country_name_swaps =   {"Iran (Islamic Republic of)":"Iran",
+							"United Republic of Tanzania":"Tanzania",
+							"Bolivia (Plurinational State of)":"Bolivia",
+							"Republic of Moldova":"Moldova",
+							"China, Hong Kong SAR":"Hong Kong",
+							"Lao People's Democratic Republic":"Laos",
+							"China, Macao SAR":"Macao",
+							"Venezuela (Bolivarian Republic of)":"Venezuela",
+							"China, Taiwan Province of":"Taiwan",
+							"Democratic People's Republic of Korea":"North Korea",
+							"Republic of Korea":"South Korea",
+							"C\xf4te d'Ivoire":"Ivory Coast",
+							"United Kingdom":"UK",
+							"United States of America":"USA",
+							"Viet Nam":"Vietnam",
+							"Russian Federation":"Russia"
+                         }
+
+	# 0          1     2          3     4             5        6     7      8       9      10      11     12      13     14
+	# Area Code  Area  Item Code  Item  Element Code  Element  Unit  Y2014  Y2014F  Y2015  Y2015F  Y2016  Y2016F  Y2017  Y2017F
+
+	production_counts = defaultdict( lambda: defaultdict(int) ) # key is country, key is category of fish, value is 2017 counts
+	import_counts = defaultdict( lambda: defaultdict(int) )
+	export_counts = defaultdict( lambda: defaultdict(int) )
+	feed_counts = defaultdict( lambda: defaultdict(int) )
+
+	sys.stderr.write("# Reading {}\n".format(csvfile) )
+	balance_csv = csv.reader(open(csvfile,'rU') )
+	for row in balance_csv:
+		country = row[1]
+		if country in country_name_swaps:
+			country = country_name_swaps.get(country)
+		item = row[3]
+		element = row[5]
+		y2017 = row[13]
+		if item in fishery_allowed_items:
+			if element   == "Production":
+				production_counts[country][item] = float(y2017)
+			elif element == "Import Quantity":
+				import_counts[country][item] = float(y2017)
+			elif element == "Export Quantity":
+				export_counts[country][item] = float(y2017)
+			elif element == "Feed":
+				feed_counts[country][item] = float(y2017)
+
+	# make header line
+	header_line = "region\titem\tproduction_2017_Gg\timports_2017_Gg\texports_2017_Gg\tfeed_2017_Gg\n"
+	sys.stdout.write(header_line)
+
+	# iterate through 
+	for country in import_counts.keys():
+		for fish_item in import_counts.get(country).keys():
+			prod_quant   = production_counts.get(country,{}).get(fish_item,0)
+			import_quant = import_counts.get(country,{}).get(fish_item,0)
+			export_quant = export_counts.get(country,{}).get(fish_item,0)
+			feed_quant = feed_counts.get(country,{}).get(fish_item,0)
+			outline = "{}\t{}\t{:.1f}\t{:.1f}\t{:.1f}\t{:.1f}\n".format(country, fish_item, prod_quant, import_quant, export_quant, feed_quant)
+			sys.stdout.write(outline)
+
+def main(argv, wayout):
+	if not len(argv):
+		argv.append("-h")
+	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=__doc__)
+	parser.add_argument('-i','--input', help="FoodBalanceSheets csv file", required=True)
+	parser.add_argument('-s','--summary', action="store_true", help="extract only summary info")
+	args = parser.parse_args(argv)
+
+	parse_balancetable_csv(args.input, args.summary)
+
+if __name__ == "__main__":
+	main(sys.argv[1:],sys.stdout)
