68296ef8f39041cc58efe5185ea7c2cea86851a6
wrf <wrfbiolum@protonmail.com>
2022-01-01 19:18:33 +0100

    replot poore2018 data

---
+++ land_usage/README.md
@@ -0,0 +1,18 @@
+# land usage #
+Replotting of some data from [Poore 2018 Reducing food’s environmental impacts through producers and consumers](https://doi.org/10.1126/science.aaq0216), based on the supplemental table 2, using the mean (not median) resampled, randomized dataset.
+
+![per_hectare_protein_Poore2018.png](https://github.com/wrf/misc-analyses/blob/master/land_usage/image/per_hectare_protein_Poore2018.png)
+
+Crustaceans come out in the lead, followed by tofu and other beans, and beef in last place, tied with lamb. However, beef cattle farming has a substantially higher CO2 footprint than for sheep.
+
+Much of this was known decades ago. Below is a plot of protein yield per hectare (adapted from a figure from Shurtleff & Aoyagi 1975 Book of Tofu ). While there may be improvements in efficiency, these would affect probably all crops as well as livestock. In any case, the principle is simple: livestock are inefficient sources of protein.
+
+![per_hectare_protein_USDA_1971.png](https://github.com/wrf/misc-analyses/blob/master/land_usage/image/per_hectare_protein_USDA_1971.png)
+
+Knowledge of the inefficient land usage was one of the arguments for vegetarianism by 19th century author Percy Shelley in his [Vindication_of_Natural_Diet](https://en.wikipedia.org/wiki/A_Vindication_of_Natural_Diet), on [page 20](https://www.gutenberg.org/files/38727/38727-h/38727-h.htm) arguing, 
+
+   "*The quantity of nutritious vegetable matter consumed in fattening the carcase of an ox, would afford ten times the sustenance, undepraving indeed, and incapable of generating disease, if gathered immediately from the bosom of the earth.*"
+
+Land usage has also changed by [removal of forest](https://www.globalforestwatch.org/map/). As much of the mass on the planet is in the [form of plants](http://www.pnas.org/lookup/doi/10.1073/pnas.1711842115), and the residence time of carbon in land plants is much longer than in the ocean, reforestation appears to be a necessary strategy for mitigating climate change.
+
+
--- land_usage/protein_yield_by_area.R
+++ land_usage/protein_yield_by_area.R
@@ -4,6 +4,9 @@
 # apparently from
 # USDA and WHO 1971
 #
+# also using data from 
+# Poore 2018 Reducing food’s environmental impacts through producers and consumers. Science
+# doi.org/10.1126/science.aaq0216
 
 library(ggplot2)
 
@@ -29,11 +32,49 @@ gp = ggplot(data=ps_data, aes(x=reorder(prot_sources, yields_kg_per_hectare), y=
          title="Per-hectare yields of protein sources",
          subtitle="Data from USDA 1971") +
     annotate(geom="text", y=yields_kg_per_hectare+15, x=rev(seq(1,9,1)), label=round(yields_kg_per_hectare) )
-ggsave("~/git/misc-analyses/land_usage/per_hectare_protein_USDA_1971.pdf", gp, device="pdf", height=5, width=8)
+ggsave("~/git/misc-analyses/land_usage/image/per_hectare_protein_USDA_1971.pdf", gp, device="pdf", height=5, width=8)
 
 
+# data from Poore 2018 Science
+food_items = c("Wheat & Rye (Bread)", "Maize (Meal)", "Barley (Beer)", "Oatmeal", 
+               "Rice", "Potatoes", "Cassava", "Cane Sugar", "Beet Sugar", 
+               "Other Pulses", "Peas", "Nuts", "Groundnuts", "Soymilk", "Tofu", 
+               "Soybean Oil", "Palm Oil", "Sunflower Oil", "Rapeseed Oil", 
+               "Olive Oil", "Tomatoes", "Onions & Leeks", "Root Vegetables", 
+               "Brassicas", "Other Vegetables", "Citrus Fruit", "Bananas", 
+               "Apples", "Berries & Grapes", "Wine", "Other Fruit", "Coffee", 
+               "Dark Chocolate", "Bovine Meat (beef herd)", "Bovine Meat (dairy herd)", 
+               "Lamb & Mutton", "Pig Meat", "Poultry Meat", 
+               "Milk", "Cheese", "Eggs", 
+               "Fish (farmed)", "Crustaceans (farmed)")
+# subset 100g protein items
+is_100g_protein = c(34, 36, 35, 43, 40, 37, 38, 41, 15, 13, 10, 11, 12 )
+# processed data from Poore2018, as m2 land per 100g protein
+food_median_land_use_m2_unit = c(2.7, 1.8, 0.9, 7.7, 2.2, 0.8, 1.3, 1.8, 1.5, 12.2, 6.7, 8.7, 7.9, 0.6, 3.4, 9.6, 2.4, 16.3, 9.4, 17.3, 0.2, 0.3, 0.3, 0.3, 0.2, 0.7, 1.4, 0.5, 2.6, 1.6, 0.9, 11.9, 53.8, 170.4, 25.9, 127.4, 13.4, 11.0, 2.1, 20.2, 5.7, 5.6, 0.8)
+food_mean_land_use_m2_unit = c(3.9, 2.9, 1.1, 7.6, 2.8, 0.9, 1.8, 2.0, 1.8, 15.6, 7.5, 13.0, 9.1, 0.7, 3.5, 10.5, 2.4, 17.7, 10.6, 26.3, 0.8, 0.4, 0.3, 0.6, 0.4, 0.9, 1.9, 0.6, 2.4, 1.8, 0.9, 21.6, 69.0, 326.2, 43.2, 369.8, 17.4, 12.2, 9.0, 87.8, 6.3, 8.4, 3.0)
+# 10000 m2 per kilogram, simplifies to 1000/x
+food_median_land_use_kg_hec = 1000 / food_median_land_use_m2_unit
+food_mean_land_use_kg_hec = 1000 / food_mean_land_use_m2_unit
+
+food_table = droplevels(data.frame( food_items, food_median_land_use_kg_hec, food_mean_land_use_kg_hec ))
+ft_protein_only = food_table[rev(is_100g_protein),]
+ft_protein_only$food_items = factor(ft_protein_only$food_items, levels = ft_protein_only$food_items)
+
+gp = ggplot(data=ft_protein_only, aes(x=food_items, y=food_mean_land_use_kg_hec) ) +
+    coord_flip() +
+    theme(axis.text.y=element_text(size=16),
+          axis.text.x=element_text(size=13),
+          axis.title.x=element_text(size=18),
+          plot.title = element_text(size=25)) +
+    scale_fill_gradient(low="#fe9929", high="#78c679") +
+    geom_col(aes(fill=food_mean_land_use_kg_hec), show.legend = FALSE) + 
+    labs(x=NULL, y="kg protein per hectare",
+         title="Per-hectare yields of protein sources",
+         subtitle="Data from Poore 2018 Science") +
+    annotate(geom="text", y=food_mean_land_use_kg_hec[is_100g_protein]+15, x=rev(seq(1,13,1)), label=round( food_mean_land_use_kg_hec[is_100g_protein] ) )
+gp
+
+ggsave("~/git/misc-analyses/land_usage/image/per_hectare_protein_Poore2018.pdf", gp, device="pdf", height=6, width=9)
 
 
-
-
-
+#
