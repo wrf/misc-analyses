34a7d739a2ae7f03f0783ff4ae1ff7b0dd69265c
wrf <wrfbiolum@protonmail.com>
2024-10-02 10:28:54 +0800

    update table from source book

--- animal_oxygen/plot_Km_vs_size.R
+++ animal_oxygen/plot_Km_vs_size.R
@@ -1,19 +1,27 @@
-#
+# replot of data from Fenchel and Finlay 1995 Ecology and Evolution in Anoxic Worlds
+# Figure 5.9 The half saturation Po for respiration of different organisms/organelles as a function of their linear dimensions.
+# https://academic.oup.com/book/54243
+# last updated 2024-10-02
 
-kmdata = read.table("~/git/misc-analyses/animal_oxygen/fenchel_finlay_1995_km_by_size.tab", header=TRUE, sep="\t")
+kmdata_file = "~/git/misc-analyses/animal_oxygen/data/fenchel_finlay_1995_km_by_size.tab"
+kmdata = read.table(kmdata_file, header=TRUE, sep="\t")
+kmdata
 
-microbes = c(11,12,13,14,15)
+is_microbe = c(11,12,13,14,15)
 
-typecols = rep("#1c9099", length(kmdata[,1]))
-typecols[microbes] = "#78c679"
+typecols = c( rep("#1c9099aa", 10), rep("#78c679aa", 4 ), "#783a26aa" )
+#typecols[is_microbe] = "#78c679aa"
 
 type_pch = rep(17,length(kmdata[,1]))
-type_pch[microbes] = 19
+type_pch[is_microbe] = 19
 
-pdf(file="~/git/misc-analyses/animal_oxygen/fenchel_finlay_1995_km_by_size.pdf", width=6, height=6)
+pdf(file="~/git/misc-analyses/animal_oxygen/images/fenchel_finlay_1995_km_by_size.pdf", width=6, height=6)
 par(mar=c(4.5,4.5,4,2))
-plot(kmdata[,3], kmdata[,4], log='xy', xlim=c(1e-4, 100), ylim=c(0.01, 100), frame.plot=FALSE, xlab="Size (cm)", ylab="Km (%AS)", main="Fenchel & Finlay 1995", col="#1c9099", pch=type_pch, cex=2, cex.lab=1.3, cex.axis=1.3)
-
+plot(kmdata[["size_cm"]], kmdata[["Km_pct_air_sat"]], frame.plot=FALSE, 
+     log='xy', xlim=c(1e-4, 100), ylim=c(0.01, 100), 
+     xlab="Size (cm)", ylab="Km (%AS)", main="Fenchel & Finlay 1995 Figure 5.9", 
+     col=typecols, pch=type_pch, cex=3, cex.lab=1.3, cex.axis=1.3)
+#text(kmdata[["size_cm"]], kmdata[["Km_pct_air_sat"]], kmdata[["Organism"]], pos=4)
 dev.off()
 
 #
