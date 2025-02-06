811cbd414f2b31d22cdc5c2730ca5c939f81a89b
wrf <wrfbiolum@protonmail.com>
2025-02-06 09:38:24 +0100

    add figure labels

--- animal_oxygen/plot_Km_vs_size.R
+++ animal_oxygen/plot_Km_vs_size.R
@@ -5,7 +5,6 @@
 
 kmdata_file = "~/git/misc-analyses/animal_oxygen/data/fenchel_finlay_1995_km_by_size.tab"
 kmdata = read.table(kmdata_file, header=TRUE, sep="\t")
-kmdata
 
 is_microbe = c(11,12,13,14,15)
 
@@ -15,13 +14,20 @@ typecols = c( rep("#1c9099aa", 10), rep("#78c679aa", 4 ), "#783a26aa" )
 type_pch = rep(17,length(kmdata[,1]))
 type_pch[is_microbe] = 19
 
-pdf(file="~/git/misc-analyses/animal_oxygen/images/fenchel_finlay_1995_km_by_size.pdf", width=6, height=6)
+
+pdf(file="~/git/misc-analyses/animal_oxygen/images/fenchel_finlay_1995_km_by_size.pdf", width=6, height=6, title="Fenchel & Finlay 1995 Figure 5.9")
+#png(file="~/git/misc-analyses/animal_oxygen/images/fenchel_finlay_1995_km_by_size.png", width=540, height=540, res = 90)
 par(mar=c(4.5,4.5,4,2))
 plot(kmdata[["size_cm"]], kmdata[["Km_pct_air_sat"]], frame.plot=FALSE, 
      log='xy', xlim=c(1e-4, 100), ylim=c(0.01, 100), 
      xlab="Size (cm)", ylab="Km (%AS)", main="Fenchel & Finlay 1995 Figure 5.9", 
      col=typecols, pch=type_pch, cex=3, cex.lab=1.3, cex.axis=1.3)
-#text(kmdata[["size_cm"]], kmdata[["Km_pct_air_sat"]], kmdata[["Organism"]], pos=4)
+text(kmdata[["size_cm"]], kmdata[["Km_pct_air_sat"]], 
+     sapply(strsplit(ifelse(!(1:15 %in% c(4,9,15)),kmdata$Organism,kmdata$type_of)," "),`[`,1), 
+     pos=ifelse(1:15 %in% c(2,3,7,9,11),2,4) )
+legend("bottomright", legend=c("Animal", "Single-cell eukaryote", "organelle"),
+       col=unique(typecols), pch=c(17,19,19), pt.cex = 2)
 dev.off()
+
 
 #
